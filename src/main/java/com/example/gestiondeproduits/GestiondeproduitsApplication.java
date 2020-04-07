285bf5b22baa753c2cd93140d03bd57598a0d606
imadben2 <imadebensotmi2015@gmail.com>
2020-04-07 14:42:21 +0100

    premier essai

---
+++ src/main/java/com/example/gestiondeproduits/GestiondeproduitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestiondeproduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiondeproduitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestiondeproduitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gestiondeproduits/GestiondeproduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestiondeproduits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiondeproduitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
